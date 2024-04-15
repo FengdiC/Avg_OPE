b64f1e897484fca1f04d2f0dd0cfdfcb5c3545a2
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2024-04-15 15:53:53 -0600

    ppo

--- hopper_ppo_biased.sh
+++ hopper_ppo_biased.sh
@@ -10,16 +10,16 @@ module load StdEnv/2023
 module load gcc opencv intel/2023.2.1 cuda/11.8 python/3.10 mpi4py
 
 SECONDS=0
-python ppo/ppo_tune.py --seed 32 --log_dir $SCRATCH/avg_discount/acrobot --env 'Acrobot-v1' --epochs 100 --save_freq 15&
-python ppo/ppo_tune.py --seed 32 --log_dir $SCRATCH/avg_discount/cartpole --env 'CartPole-v1' --epochs 100 --save_freq 15&
-python ppo/ppo_tune.py --seed 165 --log_dir $SCRATCH/avg_discount/pendulum --env 'Pendulum-v1' --epochs 100 --save_freq 15&
-python ppo/ppo_tune.py --seed 196 --log_dir $SCRATCH/avg_discount/mountaincar --env 'MountainCarContinuous-v0' --epochs 100 --save_freq 15&
+#python ppo/ppo_tune.py --seed 32 --log_dir $SCRATCH/avg_discount/acrobot --env 'Acrobot-v1' --epochs 100 --save_freq 15&
+#python ppo/ppo_tune.py --seed 32 --log_dir $SCRATCH/avg_discount/cartpole --env 'CartPole-v1' --epochs 100 --save_freq 15&
+#python ppo/ppo_tune.py --seed 165 --log_dir $SCRATCH/avg_discount/pendulum --env 'Pendulum-v1' --epochs 100 --save_freq 15&
+#python ppo/ppo_tune.py --seed 196 --log_dir $SCRATCH/avg_discount/mountaincar --env 'MountainCarContinuous-v0' --epochs 100 --save_freq 15&
 
-python ppo/ppo_tune.py --seed 189 --log_dir $SCRATCH/avg_discount/hopper --env 'Hopper-v4' --epochs 250 --save_freq 48&
+python ppo/ppo_tune.py --seed 280 --log_dir $SCRATCH/avg_discount/hopper --env 'Hopper-v4' --epochs 250 --save_freq 48&
 python ppo/ppo_tune.py --seed 394 --log_dir $SCRATCH/avg_discount/swimmer --env 'Swimmer-v4' --epochs 250 --save_freq 48&
-python ppo/ppo_tune.py --seed 790 --log_dir $SCRATCH/avg_discount/ant --env 'Ant-v4' --epochs 250 --save_freq 48&
-python ppo/ppo_tune.py --seed 566 --log_dir $SCRATCH/avg_discount/walker --env 'Walker2d-v4' --epochs 250 --save_freq 48&
-python ppo/ppo_tune.py --seed 556 --log_dir $SCRATCH/avg_discount/halfcheetah --env 'HalfCheetah-v4' --epochs 250 --save_freq 48&
+python ppo/ppo_tune.py --seed 16 --log_dir $SCRATCH/avg_discount/ant --env 'Ant-v4' --epochs 250 --save_freq 48&
+python ppo/ppo_tune.py --seed 39 --log_dir $SCRATCH/avg_discount/walker --env 'Walker2d-v4' --epochs 250 --save_freq 48&
+python ppo/ppo_tune.py --seed 280 --log_dir $SCRATCH/avg_discount/halfcheetah --env 'HalfCheetah-v4' --epochs 250 --save_freq 48&
 
 echo "Baseline job $seed took $SECONDS"
 sleep 72h

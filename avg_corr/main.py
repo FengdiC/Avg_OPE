5dd1ce93a5f405dab8d3d61c8337f7ab20e9e75d
Fengdi Che <fengdic@beluga4.int.ets1.calculquebec.ca>
2024-05-03 18:24:36 -0400

    beluga

--- avg_corr/main.py
+++ avg_corr/main.py
@@ -339,7 +339,7 @@ def tune():
                 name = [str(s) for s in name]
                 name.append(str(seed))
                 print("hyperparam", '-'.join(name))
-                logger.logkv("hyperparam", '-'.join(name))
+                logger.logkv("hyperparam", '-'.join(name)+'\n')
                 for n in range(cv.shape[0]):
                     logger.logkv(str(n * args.steps), cv[n])
                 logger.dumpkvs()
@@ -351,12 +351,12 @@ def tune():
             name = [str(s) for s in name]
             name_1 = name +['mean']
             name_2 = name+ ['var']
-            logger.logkv("hyperparam", '-'.join(name_1))
+            logger.logkv("hyperparam", '-'.join(name_1)+'\n')
             for n in range(ret.shape[0]):
                 logger.logkv(str(n * args.steps), ret[n])
             logger.dumpkvs()
 
-            logger.logkv("hyperparam", '-'.join(name_2))
+            logger.logkv("hyperparam", '-'.join(name_2)+'\n')
             for n in range(ret.shape[0]):
                 logger.logkv(str(n * args.steps), var[n])
             logger.dumpkvs()

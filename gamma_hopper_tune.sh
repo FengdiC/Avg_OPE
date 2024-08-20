e6539ea95ba6bf020bf7d8f890129946e2637586
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2024-08-19 18:27:37 -0600

    hopper_tune

--- gamma_hopper_tune.sh
+++ gamma_hopper_tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=3600M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-12:00
+#SBATCH --time=0-72:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=def-ashique
 #SBATCH --array=1-480
@@ -17,7 +17,7 @@ echo
 
 python avg_corr/gamma.py --path './exper/hopper.pth' --env 'Hopper-v4' \
 --log_dir $SCRATCH/avg_gamma/hopper/ --array $SLURM_ARRAY_TASK_ID \
---steps 5 --epoch 50000 --max_len 100 --seed 280 &
+--steps 5 --epoch 10000 --max_len 100 --seed 280 &
 
 echo "Baseline job $seed took $SECONDS"
 sleep 72h
--- mse_hopper_tune.sh
+++ mse_hopper_tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=3600M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-12:00
+#SBATCH --time=0-72:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=def-ashique
 #SBATCH --array=1-480
@@ -16,7 +16,7 @@ SECONDS=0
 echo
 
 python avg_corr/main.py --path './exper/hopper.pth' --env 'Hopper-v4' --array $SLURM_ARRAY_TASK_ID \
---log_dir $SCRATCH/avg_mse/hopper/ --steps 5 --epoch 50000 --max_len 100 --seed 280 &
+--log_dir $SCRATCH/avg_mse/hopper/ --steps 5 --epoch 10000 --max_len 100 --seed 280 &
 
 echo "Baseline job $seed took $SECONDS"
 sleep 72h

50ed3f33417f6edc3ade5d81a9e61fb0780a6686
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2024-08-24 18:23:28 -0600

    td

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=0-3:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=def-ashique
-#SBATCH --array=1-135
+# haha  SBATCH --array=1-135
 
 # salloc --cpus-per-task=1 --mem=3600M --time=0-3:00 --account=def-ashique
 # Did not tune for three discount factors
@@ -16,8 +16,10 @@ module load gcc opencv intel/2023.2.1 cuda/11.8 python/3.10 mpi4py
 SECONDS=0
 echo
 
-python avg_corr/run_classic.py --log_dir $SCRATCH/avg_corr/classic/ \
---array $SLURM_ARRAY_TASK_ID  --steps 5 --epoch 2000 --max_len 50 &
+python avg_corr/run_cartpole_td.py --log_dir $SCRATCH/avg_corr/td_err/ --steps 5 --epoch 2000 --max_len 50 
+
+#python avg_corr/run_classic.py --log_dir $SCRATCH/avg_corr/classic/ \
+#--array $SLURM_ARRAY_TASK_ID  --steps 5 --epoch 2000 --max_len 50 &
 
 #python avg_corr/run_mujoco.py --log_dir $SCRATCH/avg_corr/classic/ \
 #--array $SLURM_ARRAY_TASK_ID --steps 5 --epoch 50000 --max_len 100 &

ba8062119dd3a963f7e9f9677ca0f537c2db25ae
Bryan Chan <bryanchan13@gmail.com>
2024-09-05 18:51:04 -0600

    Update

--- disc_cop/envs.py
+++ disc_cop/envs.py
@@ -10,45 +10,45 @@ from disc_cop.constants import REPO_PATH
 
 ENVS = {
     "mujoco": {
-        # "ant": ("Ant-v4", "{}/exper/ant.pth".format(REPO_PATH)),
-        # "halfcheetah": (
-        #     "HalfCheetah-v4",
-        #     "{}/exper/halfcheetah_1.pth".format(REPO_PATH),
-        # ),
+        "ant": ("Ant-v4", "{}/exper/ant.pth".format(REPO_PATH)),
+        "halfcheetah": (
+            "HalfCheetah-v4",
+            "{}/exper/halfcheetah_1.pth".format(REPO_PATH),
+        ),
         "hopper": ("Hopper-v4", "{}/exper/hopper.pth".format(REPO_PATH)),
-        # "swimmer": ("Swimmer-v4", "{}/exper/swimmer.pth".format(REPO_PATH)),
-        # "walker": ("Walker2d-v4", "{}/exper/walker.pth".format(REPO_PATH)),
+        "swimmer": ("Swimmer-v4", "{}/exper/swimmer.pth".format(REPO_PATH)),
+        "walker": ("Walker2d-v4", "{}/exper/walker.pth".format(REPO_PATH)),
     },
     "classic_control": {
-        # "acrobot": ("Acrobot-v1", "{}/exper/acrobot.pth".format(REPO_PATH)),
+        "acrobot": ("Acrobot-v1", "{}/exper/acrobot.pth".format(REPO_PATH)),
         "cartpole": ("CartPole-v1", "{}/exper/cartpole.pth".format(REPO_PATH)),
-        # "mountain_car": (
-        #     "MountainCarContinuous-v0",
-        #     "{}/exper/mountaincar.pth".format(REPO_PATH),
-        # ),
+        "mountain_car": (
+            "MountainCarContinuous-v0",
+            "{}/exper/mountaincar.pth".format(REPO_PATH),
+        ),
     },
 }
 
 ENV_ID_TO_NAME = {
-    # "Ant-v4": "ant",
-    # "HalfCheetah-v4": "halfcheetah",
+    "Ant-v4": "ant",
+    "HalfCheetah-v4": "halfcheetah",
     "Hopper-v4": "hopper",
-    # "Swimmer-v4": "swimmer",
-    # "Walker2d-v4": "walker",
-    # "Acrobot-v1": "acrobot",
+    "Swimmer-v4": "swimmer",
+    "Walker2d-v4": "walker",
+    "Acrobot-v1": "acrobot",
     "CartPole-v1": "cartpole",
-    # "MountainCarContinuous-v0": "mountain_car",
+    "MountainCarContinuous-v0": "mountain_car",
 }
 
 ENV_TO_FAMILY = {
-    # "ant": "mujoco",
-    # "halfcheetah": "mujoco",
+    "ant": "mujoco",
+    "halfcheetah": "mujoco",
     "hopper": "mujoco",
-    # "swimmer": "mujoco",
-    # "walker": "mujoco",
-    # "acrobot": "classic_control",
+    "swimmer": "mujoco",
+    "walker": "mujoco",
+    "acrobot": "classic_control",
     "cartpole": "classic_control",
-    # "mountain_car": "classic_control",
+    "mountain_car": "classic_control",
 }
 
 ENV_FAMILY_SPECIFICS = {

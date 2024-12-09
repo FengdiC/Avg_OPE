4371c62f854cf7d1e6ecdd07aba106b5afb647cf
Fengdi <fengdi.che@mail.mcgill.ca>
2024-12-09 08:32:26 -0700

    run

--- run/run_mujoco.py
+++ run/run_mujoco.py
@@ -69,13 +69,13 @@ def run_mujoco():
                 'buffer_size', 16000, 'seed', seed, 'env', env]
         name = '-'.join(str(x) for x in name)
 
-        with open(args.data_dir+'/dataset/' + name + '.pkl', 'wb') as outp:
+        with open(args.data_dir+'/dataset/' + name + '.pkl', 'r') as outp:
             buf = pickle.load(outp)
         name = ['discount_factor', 0.8, 'random_weight', random_weight, 'max_length', length,
                 'buffer_size', 16000, 'seed', seed + 1314, 'env', env]
         name = '-'.join(str(x) for x in name)
 
-        with open(args.data_dir +'/dataset_test/'+ name + '.pkl', 'wb') as outp:
+        with open(args.data_dir +'/dataset_test/'+ name + '.pkl', 'r') as outp:
             buf_test = pickle.load(outp)
         for size in size_lists:
             buf.ptr, buf.max_size = size, size
@@ -128,13 +128,13 @@ def run_mujoco():
                 'buffer_size', 16000, 'seed', seed, 'env', env]
         name = '-'.join(str(x) for x in name)
 
-        with open(args.data_dir+'/dataset/' + name + '.pkl', 'wb') as outp:
+        with open(args.data_dir+'/dataset/' + name + '.pkl', 'r') as outp:
             buf = pickle.load(outp)
         name = ['discount_factor', 0.8, 'random_weight', random_weight, 'max_length', length,
                 'buffer_size', 16000, 'seed', seed + 1314, 'env', env]
         name = '-'.join(str(x) for x in name)
 
-        with open(args.data_dir +'/dataset_test/'+ name + '.pkl', 'wb') as outp:
+        with open(args.data_dir +'/dataset_test/'+ name + '.pkl', 'r') as outp:
             buf_test = pickle.load(outp)
         for size in size_lists:
             buf.ptr, buf.max_size = size, size

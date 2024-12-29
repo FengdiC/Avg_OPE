a956a85ef8375fa1f2b98bfced6f8546055dc926
Fengdi <fengdi.che@mail.mcgill.ca>
2024-12-29 12:31:15 -0700

    dice

--- dice_rl/scripts/run_neural_dice.py
+++ dice_rl/scripts/run_neural_dice.py
@@ -20,8 +20,12 @@ from absl import app
 from absl import flags
 
 import numpy as np
-import os
-import sys
+import os, sys, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+granddir = os.path.dirname(parentdir)
+sys.path.insert(0, parentdir)
+sys.path.insert(0, granddir)
 import tensorflow.compat.v2 as tf
 tf.compat.v1.enable_v2_behavior()
 import pickle, csv

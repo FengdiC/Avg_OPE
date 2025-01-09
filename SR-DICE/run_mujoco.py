a82c6069190e978bb6a302566126e6536f282d54
Fengdi <fengdi.che@mail.mcgill.ca>
2025-01-08 22:06:48 -0700

    mis

--- SR-DICE/run_mujoco.py
+++ SR-DICE/run_mujoco.py
@@ -160,7 +160,7 @@ def run(args,env_name,seed,size,length,random_weight,discount_factor,num_steps,c
         torch.save(
             {'encoder_dict':ope.encoder_decoder.state_dict(),
              'weight_dict': ope.W},
-            dir
+            f"{dir}/{name}-final.pt"
         )
         print("model saved")
     return train_results, test_results

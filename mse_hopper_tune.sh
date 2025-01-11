186e1c7a55cb6e00d1f80b017cb2eebe2bb3ce93
Fengdi <fengdi.che@mail.mcgill.ca>
2025-01-10 18:11:37 -0700

    no dataset

--- mse_hopper_tune.sh
+++ mse_hopper_tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=3600M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-72:00
+#SBATCH --time=0-3:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=def-ashique
 #SBATCH --gpus-per-node=1
